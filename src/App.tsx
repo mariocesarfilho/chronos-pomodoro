cf78f8f54586eeaa39aa8aaf316253db9b85e32b
Mário César <mariocesarmoliveirafilho@gmail.com>
2025-09-19 16:00:29 -0300

    finalizando componente Heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { TimerIcon } from 'lucide-react';
 import { Heading } from './components/Heading';
 import './styles/global.css';
 import './styles/theme.css';
@@ -7,11 +8,18 @@ export function App() {
     return (
         //React fragment - quando você não quer colocar um elemento Pai
         <>
-            <Heading />
+            <Heading>
+                Propriedade
+                
+                <button>
+                    <TimerIcon />
+                </button>
+            </Heading>             
+            
             <h1>Hello, World!</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus 
-                aliquam ad ex, in cum atque culpa veniam ut sunt doloribus et? Ducimus quisquam 
-                laboriosam dicta cupiditate, quam esse explicabo saepe.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus 
+            aliquam ad ex, in cum atque culpa veniam ut sunt doloribus et? Ducimus quisquam 
+            laboriosam dicta cupiditate, quam esse explicabo saepe.</p>
         </>
     );
 }
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,7 +1,32 @@
-export function Heading() {
+import styles from './Heading.module.css';
+
+
+type HeadingProps = {
+    //Tudo que o react aceita como children está nesse tipo
+    children: React.ReactNode;
+}
+
+export function Heading({ children }: HeadingProps) {
+    /**
+     * DESTRUTURAÇÃO - Em JavaScript
+     * 
+     * 
+     * Uma opção para utilizar
+     * 
+     * const children = props.children;
+     * 
+     * ----
+     * 
+     * outra forma de fazer
+     * 
+     * const { children } = props
+     * 
+     * 
+     * -- outra forma é utilizar direto no parâmetro da função, e é a forma que vamos usar
+     */
     return (
         <>
-            <h1> Olá mundo</h1>
+            <h1 className={styles.heading}>{children}</h1>
         </>
     );
 }
