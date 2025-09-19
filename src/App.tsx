746b7b4b5619fe7b7b308899817696bb626d6160
Mário César <mariocesarmoliveirafilho@gmail.com>
2025-09-19 15:08:35 -0300

    propriedades e children ddos componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Heading } from './components/Heading';
 import './styles/global.css';
 import './styles/theme.css';
 
@@ -6,6 +7,8 @@ export function App() {
     return (
         //React fragment - quando você não quer colocar um elemento Pai
         <>
+            <Heading>Olá Mundo! 1</Heading>
+            <Heading>Olá Mundo! 2</Heading>
             <h1>Hello, World!</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus 
                 aliquam ad ex, in cum atque culpa veniam ut sunt doloribus et? Ducimus quisquam 
---
+++ src/components/Heading.tsx
@@ -0,0 +1,10 @@
+import styles from './Heading.module.css';
+
+
+export function Heading(props) {
+    return (
+        <>
+            <h1 className={styles.heading}>{props.children}</h1>
+        </>
+    );
+}
